e5eaf70954e73d28935454615c880c3c0f99f4c4
Javan Rasokat <mail@javan.de>
2021-06-25 15:26:27 +0200

    fix

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2014-2021 Bjoern Kimminich.
  * SPDX-License-Identifier: MIT
  */
-
+require('tcell-agent')
 require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
   server.start()
