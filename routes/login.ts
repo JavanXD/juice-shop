b04217eb656b03ac873365b74e70931de1a765d6
Javan Rasokat <mail@javan.de>
2021-07-02 17:43:18 +0200

    fix wrong parameter order

--- routes/login.ts
+++ routes/login.ts
@@ -15,10 +15,9 @@ const tCellHooks = require('tcell-hooks').v1
 module.exports = function login () {
   function afterLogin (user, res, next, req) {
     verifyPostLoginChallenges(user) // vuln-code-snippet hide-line
-    const token = security.authorize(user)
-    tCellHooks.sendExpressLoginEventSuccess(user.data.email, token, req)
     models.Basket.findOrCreate({ where: { UserId: user.data.id }, defaults: {} })
       .then(([basket]) => {
+        const token = security.authorize(user)
         user.bid = basket.id // keep track of original basket
         security.authenticatedUsers.put(token, user)
         res.json({ authentication: { token, bid: basket.id, umail: user.data.email } })
@@ -43,9 +42,10 @@ module.exports = function login () {
             }
           })
         } else if (user.data?.id) {
+          tCellHooks.sendExpressLoginEventSuccess(req, req.body.email, req.cookies.io)
           afterLogin(user, res, next, req)
         } else {
-          tCellHooks.sendExpressLoginEventFailure(req.body.email, (req.cookies?.token ? req.cookies.token : ''), req, false)
+          tCellHooks.sendExpressLoginEventFailure(req, req.body.email, req.cookies.io)
           res.status(401).send(res.__('Invalid email or password.'))
         }
       }).catch(error => {
